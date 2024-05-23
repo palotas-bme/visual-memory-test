bc2478cbda933648082d8b437523dbe004bd50b2
Palotás M. Péter <palotas@edu.bme.hu>
2024-05-23 15:28:34 +0200

    add R script to create picot table for JASP

---
+++ pivot.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(dtplyr)
+library(stringr)
+
+headers = c("ID",
+            "Age",
+            "Correct [n=5]",
+            "Correct [n=9]",
+            "Correct [n=13]",
+            "Reaction time [n=5]",
+            "Reaction time [n=9]",
+            "Reaction time [n=13]",
+            "Total correct"
+            )
+
+
+df = read_tsv('./data/_final_mergedfile.csv')
+df_long = df %>% select(id, age, pic_num, is_correct, mouse.time ) %>% 
+  mutate(is_correct=as.logical(is_correct), mouse.time = as.double(gsub('\\[([0-9]+\\.[0-9]+).*', '\\1', mouse.time))) %>% 
+  filter(!is.na(is_correct), !is.na(pic_num)) 
+
+df_wide = df_long %>% 
+  group_by(id, pic_num) %>% 
+  summarise(correct=sum(is_correct), mean_time=mean(mouse.time), age=mean(age)) %>% 
+  pivot_wider(names_from = pic_num, values_from = c(correct, mean_time)) %>% 
+  mutate(sum_correct=sum(across(starts_with("correct")))) 
+
+names(df_wide) = headers
+
+write_csv(df_wide, "visual_memory_pivot.csv")
+write_csv(df_long, "visual_memory_pivot_long.csv")
